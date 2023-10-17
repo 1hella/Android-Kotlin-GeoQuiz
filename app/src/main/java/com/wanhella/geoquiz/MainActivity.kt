4464381df81151426fda69450fafd2c8b1f2705d
Stephen Wanhella <stephen.wanhella@gmail.com>
2023-10-17 14:14:38 -0700

    Chapter 8 - SDK Versions and Compatibility

--- app/src/main/java/com/wanhella/geoquiz/MainActivity.kt
+++ app/src/main/java/com/wanhella/geoquiz/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.wanhella.geoquiz
 
 import android.app.Activity
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.wanhella.geoquiz.databinding.ActivityMainBinding
 
@@ -55,6 +59,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         updateQuestion()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            blurCheatButton()
+        }
     }
 
     private fun updateQuestion() {
@@ -73,6 +80,16 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun blurCheatButton() {
+        val effect = RenderEffect.createBlurEffect(
+            10.0f,
+            10.0f,
+            Shader.TileMode.CLAMP
+        )
+        binding.cheatButton.setRenderEffect(effect)
     }
 
 
